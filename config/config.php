8957768f1339eaefb7e19d0418e97255c5501f40
ines.fialho <inesespinhaa@gmail.com>
2020-02-29 16:51:38 +0000

    Database Class

--- config/config.php
+++ config/config.php
@@ -1,3 +1,16 @@
 <?php
-// configuration of database parameters
+
+/*
+** configuration of database parameters
+** define method to constants that don't change
+*/
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "QWERTY123");
+define("DB_NAME", "joblister");
+
+// Another global variables of the project
+define("SITE_TITLE", "JobFinder");
+
+
 ?>
--- templates/inc/header.php
+++ templates/inc/header.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>JobLister</title>
+    <title><?php echo SITE_TITLE; ?></title>
     <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
     <div class="container">
         <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-            <h5 class="my-0 mr-md-auto font-weight-normal">JobLister</h5>
+            <h5 class="my-0 mr-md-auto font-weight-normal"><?php echo SITE_TITLE; ?></h5>
             <nav class="my-2 my-md-0 mr-md-3">
                 <a role="presentation" class="p-2 text-dark" href="index.php">Home</a>
                 <a role="presentation" class="p-2 text-dark" href="create.php">Create Listing</a>
